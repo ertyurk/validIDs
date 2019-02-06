f622f6ae81690ac1c5600495b2fc8de196e208e2
ertyurk <30368068+ertyurk@users.noreply.github.com>
2019-02-06 16:50:27 +0200

    Create IBAN_check.py

---
+++ IBAN_check.py
@@ -0,0 +1,41 @@
+IBAN_CODES = {
+    "A": "10",
+    "B": "11",
+    "C": "12",
+    "D": "13",
+    "E": "14",
+    "F": "15",
+    "G": "16",
+    "H": "17",
+    "I": "18",
+    "J": "19",
+    "K": "20",
+    "L": "21",
+    "M": "22",
+    "N": "23",
+    "O": "24",
+    "P": "25",
+    "Q": "26",
+    "R": "27",
+    "S": "28",
+    "T": "29",
+    "U": "30",
+    "V": "31",
+    "W": "32",
+    "X": "33",
+    "Y": "34",
+    "Z": "35"
+}
+
+
+def checkIBAN(iban):
+
+    if isinstance(iban, str):
+        x = IBAN_CODES[iban[0]]
+        k = IBAN_CODES[iban[1]]
+        iban = iban[4:] + x + k + iban[2] + iban[3]
+        iban = int(iban)
+        return iban % 97 == 1
+    return False
+
+
